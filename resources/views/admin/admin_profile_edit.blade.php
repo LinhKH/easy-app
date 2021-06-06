9eac12d45e35ac80ccb7eca6e086fd7a3f9b21d5
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-06-06 14:02:06 +0700

    100. Admin Profile & Image Update Part 3

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <div class="container-full">
     <section class="content">
@@ -39,12 +40,12 @@
                                             <div class="col-6">
                                                 <h5>File Input Field <span class="text-danger">*</span></h5>
                                                 <div class="controls">
-                                                    <input type="file" name="file" class="form-control" required="">
+                                                    <input type="file" name="file" id="image" class="form-control" required="">
                                                     <div class="help-block"></div>
                                                 </div>
                                             </div>
                                             <div class="col-6">
-                                                <img class="rounded-circle" style="width: 100px; height:100px;"
+                                                <img class="rounded-circle" style="width: 100px; height:100px;" id="showImage"
                                                     src="{{ !empty($editData->profile_photo_path) ? url('upload/admin_images/' . $editData->profile_photo_path) : url('upload/no_image.jpg') }}"
                                                     alt="User Avatar">
                                             </div>
@@ -68,4 +69,15 @@
 
     </section>
 </div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#image').change(function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#showImage').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        });
+    });
+</script>
 @endsection
